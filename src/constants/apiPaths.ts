e1629813378b45d223f48e5bdeca3ae90c473fb7
Tolulope Taiwo Sheriff <oluwalunar@gmail.com>
2025-03-15 23:28:12 +0100

    Updated the api paths.

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,9 +2,10 @@ const API_PATHS = {
   //product: "https://.execute-api.eu-west-1.amazonaws.com/dev",https://pj8m5nmg5m.execute-api.us-east-1.amazonaws.com/prod/products/
   //product: "https://i58jmttgyj.execute-api.us-east-1.amazonaws.com/prod/products",https://pj8m5nmg5m.execute-api.us-east-1.amazonaws.com/prod/products
   // product: "https://pj8m5nmg5m.execute-api.us-east-1.amazonaws.com/prod",https://c00aj28k2m.execute-api.us-east-1.amazonaws.com/prod/products
+  // import: "https://5uj67empu0.execute-api.us-east-1.amazonaws.com/prod",
   product: "https://c00aj28k2m.execute-api.us-east-1.amazonaws.com/prod/",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://5uj67empu0.execute-api.us-east-1.amazonaws.com/prod",
+  import: "https://tpv5ydqmbg.execute-api.us-east-1.amazonaws.com/prod/",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
