598be70e66f5db2a3d91f9866455a60a7ffd7169
Tolulope Taiwo Sheriff <oluwalunar@gmail.com>
2025-03-15 14:01:36 +0100

    Update Api Paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,8 @@
 const API_PATHS = {
   //product: "https://.execute-api.eu-west-1.amazonaws.com/dev",https://pj8m5nmg5m.execute-api.us-east-1.amazonaws.com/prod/products/
   //product: "https://i58jmttgyj.execute-api.us-east-1.amazonaws.com/prod/products",https://pj8m5nmg5m.execute-api.us-east-1.amazonaws.com/prod/products
-  product: "https://pj8m5nmg5m.execute-api.us-east-1.amazonaws.com/prod",
+  // product: "https://pj8m5nmg5m.execute-api.us-east-1.amazonaws.com/prod",https://c00aj28k2m.execute-api.us-east-1.amazonaws.com/prod/products
+  product: "https://c00aj28k2m.execute-api.us-east-1.amazonaws.com/prod/",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://5uj67empu0.execute-api.us-east-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
